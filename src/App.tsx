d0d563cf9642769d97f95b8bf034b0f249d019cf
Artem_Mykhailenko <Artemlend@gmail.com>
2025-01-21 03:54:46 +0200

    upcoming pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import HeroSection from "./components/HeroSection/HeroSection";
 import StatsSection from "./components/StatsSection/StatsSection";
@@ -13,47 +12,9 @@ import { ThirdwebProvider } from "@thirdweb-dev/react";
 import Projects from "./page/Projects/Projects";
 import TokenDetail from "./components/Token/TokenDetail";
 import InvestmentsSection from "./components/InvestmentsSection/InvestmentsSection";
-import UpcomingProjects, {
-  UpcomingProjectDetails,
-} from "./page/UpcomingProjects/UpcomingProject";
-
-interface UpcomingProject {
-  id: string;
-  name: string;
-  description: string;
-  logoUrl: string;
-  bannerUrl: string;
-  startDate: string;
-  targetRaise: number;
-  totalParticipants: number;
-  timeUntilStart: string;
-  chain: string;
-  status: string;
-  categories: string[];
-}
+import { UpcomingProjectDetails } from "./page/UpcomingProjects/UpcomingProject";
 
 function App() {
-  const [projects, setProjects] = useState<UpcomingProject[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const BASE_URL = import.meta.env.VITE_BASE_URL;
-
-  useEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const response = await fetch(`${BASE_URL}/api/projects`);
-        if (!response.ok) throw new Error("Failed to fetch projects");
-        const data = await response.json();
-        setProjects(data);
-      } catch (error) {
-        console.error("Error loading projects:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchProjects();
-  }, []);
-
   return (
     <ThirdwebProvider
       clientId="d28d89a66e8eb5e73d6a9c8eeaa0645a"
--- src/components/UpcomingProjects/UpcomingProjects.tsx
+++ src/components/UpcomingProjects/UpcomingProjects.tsx
@@ -7,7 +7,7 @@ import {
   ChevronLeft,
   ChevronRight,
 } from "lucide-react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styles from "./UpcomingProjects.module.css";
 
 interface UpcomingProject {
